51f285e9aae5f56803bc7036ed7fb29968cf7195
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-10-27 22:29:33 +0300

    Styling Add component

--- client/src/components/add/Add.tsx
+++ client/src/components/add/Add.tsx
@@ -1,5 +1,7 @@
 import { GridColDef } from '@mui/x-data-grid';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+// import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import './add.scss';
 
 type Props = {
     slug: string;
@@ -8,37 +10,37 @@ type Props = {
 };
 
 const Add = (props: Props) => {
-    const queryClient = useQueryClient();
+    // const queryClient = useQueryClient();
 
-    const mutation = useMutation({
-        mutationFn: () => {
-            return fetch(`http://localhost:8800/api/${props.slug}s`, {
-                method: "post",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    id: 111,
-                    img: "",
-                    lastName: "Hello",
-                    firstName: "Test",
-                    email: "testme@gmail.com",
-                    phone: "123 456 789",
-                    createdAt: "01.02.2023",
-                    verified: true,
-                }),
-            });
-        },
-        onSuccess: () => {
-            queryClient.invalidateQueries([`all${props.slug}s`]);
-        },
-    });
+    // const mutation = useMutation({
+    //     mutationFn: () => {
+    //         return fetch(`http://localhost:8800/api/${props.slug}s`, {
+    //             method: "post",
+    //             headers: {
+    //                 Accept: "application/json",
+    //                 "Content-Type": "application/json",
+    //             },
+    //             body: JSON.stringify({
+    //                 id: 111,
+    //                 img: "",
+    //                 lastName: "Hello",
+    //                 firstName: "Test",
+    //                 email: "testme@gmail.com",
+    //                 phone: "123 456 789",
+    //                 createdAt: "01.02.2023",
+    //                 verified: true,
+    //             }),
+    //         });
+    //     },
+    //     onSuccess: () => {
+    //         queryClient.invalidateQueries([`all${props.slug}s`]);
+    //     },
+    // });
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        mutation.mutate();
+        // mutation.mutate();
         props.setOpen(false);
     };
 
